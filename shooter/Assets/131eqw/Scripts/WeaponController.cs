99e404c68cd61839bad17f1c387f63c961db6fa1
yingtingyang <yangy389@newschool.edu>
2016-09-20 10:53:46 -0400

    commit

---
+++ shooter/Assets/131eqw/Scripts/WeaponController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponController : MonoBehaviour
+{
+
+    public GameObject bullet;
+    private bool canFire = true;
+    public float cooldown = .4f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.T) && canFire == true)
+        {
+            canFire = false;
+            StartCoroutine("reload");
+
+
+            GameObject temp = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+            temp.GetComponent<Rigidbody>().AddForce(Vector3.up * 2000);
+        }
+
+    }
+
+    IEnumerator reload()
+    {
+        yield return new WaitForSeconds(cooldown);
+        canFire = true;
+    }
+}
